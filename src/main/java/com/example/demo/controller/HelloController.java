a05fc2b8312502d3a384639cf13223c5fe18fe7b
jack6177733651 <40873770+jack6177733651@users.noreply.github.com>
2024-04-23 11:42:57 -0400

    Merge jack into master

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "hello world! from remote";
+        return "hello world! from jack";
     }
 }
