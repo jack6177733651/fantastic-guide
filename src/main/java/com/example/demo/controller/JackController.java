df4a39c68f49d01f8250095e40063df0a6f2069b
jack6177733651 <jackshi3@gmail.com>
2024-04-23 11:29:37 -0400

    Changing jackController

--- src/main/java/com/example/demo/controller/JackController.java
+++ src/main/java/com/example/demo/controller/JackController.java
@@ -1,4 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class JackController {
+
+    @GetMapping("/jack")
+    public String sayJack() {
+        return "jack";
+    }
 }
